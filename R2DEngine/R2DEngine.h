2fd0c76c5fe5b80321ebfa11aebf5d5cde953758
Downey, Eric <edowney@icc11077.local>
2016-01-08 14:24:57 -0500

    Initial Commit

---
+++ R2DEngine/R2DEngine.h
@@ -0,0 +1,19 @@
+//
+//  R2DEngine.h
+//  R2DEngine
+//
+//  Created by Downey, Eric on 1/8/16.
+//  Copyright © 2016 Eric Downey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for R2DEngine.
+FOUNDATION_EXPORT double R2DEngineVersionNumber;
+
+//! Project version string for R2DEngine.
+FOUNDATION_EXPORT const unsigned char R2DEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <R2DEngine/PublicHeader.h>
+
+
